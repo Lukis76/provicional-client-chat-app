be9b0b362179b359d6c2116929862d6ae7ae2271
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-02-26 14:54:31 -0300

    prettier

--- src/components/chat/navbar/modal/conversationModal.tsx
+++ src/components/chat/navbar/modal/conversationModal.tsx
@@ -4,73 +4,61 @@ import {
   SearchUser,
   SearchUsersData,
   SearchUsersInput,
-} from '@types'
-import { Participants } from './participants'
-import { UserSearchList } from './useSearchList'
-import { useLazyQuery } from '@apollo/client'
-import { SvgLoading } from '@assets/svg'
-import { useRoom, useViewConversation } from '@hooks/index'
-import { operations } from '@GraphQL/index'
-import {
-  Dispatch,
-  FC,
-  FormEvent,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from 'react'
-import { toast } from 'react-hot-toast'
-import { authUserContext } from '@context/index'
-import css from '@styles/chat/navbar/modal/modal.module.css'
-import style from '@styles/chat/navbar/modal/form_modal.module.css'
+} from "@types";
+import { Participants } from "./participants";
+import { UserSearchList } from "./useSearchList";
+import { useLazyQuery } from "@apollo/client";
+import { SvgLoading } from "@assets/svg";
+import { useRoom, useViewConversation } from "@hooks/index";
+import { operations } from "@GraphQL/index";
+import { Dispatch, FC, FormEvent, SetStateAction, useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { authUserContext } from "@context/index";
+import css from "@styles/chat/navbar/modal/modal.module.css";
+import style from "@styles/chat/navbar/modal/form_modal.module.css";
 
 type TypeConversationModal = {
-  close: Dispatch<SetStateAction<boolean>>
-  conversations: Array<ConversationFE>
-  editingConversation: ConversationFE | null
-}
+  close: Dispatch<SetStateAction<boolean>>;
+  conversations: Array<ConversationFE>;
+  editingConversation: ConversationFE | null;
+};
 
 export type TypeState = {
-  username: string
-  participants: Array<SearchUser>
-  existConversation: ConversationFE | null
-}
+  username: string;
+  participants: Array<SearchUser>;
+  existConversation: ConversationFE | null;
+};
 
 export const ConversationModal: FC<TypeConversationModal> = ({
   close,
   conversations,
   editingConversation,
 }) => {
-  console.log("🚀 ~ file: conversationModal.tsx:45 ~ editingConversation", editingConversation)
-  //-----------------------------------------------
+  //----------------------------------------------
   const [state, setState] = useState<TypeState>({
-    username: '',
+    username: "",
     participants: [],
     existConversation: null,
-  })
-  //-----------------------------------------------------
-  const { user } = useContext(authUserContext)
-  const { onViewConversation } = useViewConversation()
-  //---------------------------------------------------------------------
-  const [
-    searchUsers,
-    { data: dataSearch, loading: loadingSearch, error: searchUserError },
-  ] = useLazyQuery<SearchUsersData, SearchUsersInput>(
+  });
+  //-------------------------------------------
+  const { user } = useContext(authUserContext);
+  const { onViewConversation } = useViewConversation();
+  //----------------------------------------------------------------------------------------------
+  const [searchUsers, { data, loading, error }] = useLazyQuery<SearchUsersData, SearchUsersInput>(
     operations.user.query.SEARCH_USERS
-  )
-  //---------------------------------------------------------------------
+  );
+  //-----------------------------------------------------------
   const { onCreatedRoom, loadingCreateConversation } = useRoom(
     conversations,
     state,
     setState,
     close
-  )
+  );
   //---------------------------------------------------
   const handleSubmitSearch = async (e: FormEvent) => {
-    e.preventDefault()
-    searchUsers({ variables: { username: state.username } })
-  }
+    e.preventDefault();
+    searchUsers({ variables: { username: state.username } });
+  };
   //----------------------------------------------------------
   useEffect(() => {
     if (state.existConversation) {
@@ -79,37 +67,36 @@ export const ConversationModal: FC<TypeConversationModal> = ({
         participants: prev.existConversation
           ? prev.existConversation.participants.map((p) => p.user)
           : [],
-      }))
-      return
+      }));
+      return;
     }
-  }, [editingConversation])
-  //------------------------------------------------------------
+  }, [editingConversation]);
+  //----------------------------------
   const onConversationClick = () => {
-    if (!state.existConversation) return
+    if (!state.existConversation) return;
 
     const { hasSeenLatestMsg } = state.existConversation.participants.find(
       (p) => p.user.id === user?.id
-    ) as ConversationParticipant
+    ) as ConversationParticipant;
 
-    onViewConversation(state.existConversation.id, hasSeenLatestMsg, user)
-    close((state) => !state)
-  }
-
+    onViewConversation(state.existConversation.id, hasSeenLatestMsg, user);
+    close((state) => !state);
+  };
+  //--------------------------
   const handleClose = () => {
     setState((prev) => ({
       ...prev,
-      username: '',
+      username: "",
       participants: [],
-    }))
-    close((state) => !state)
+    }));
+    close((state) => !state);
+  };
+  //---------------------------------------
+  if (error) {
+    toast.error(error.message.toString());
+    return null;
   }
-  //-------------------------------------------------------------------
-
-  if (searchUserError) {
-    toast.error(searchUserError.message.toString())
-    return null
-  }
-  //-----------------------------------------------------------------------
+  //---------------------------------------
   return (
     <div className={`${css.modal}`}>
       <button className={`${css.modal}`} onClick={handleClose} />
@@ -122,29 +109,23 @@ export const ConversationModal: FC<TypeConversationModal> = ({
           </button>
         </div>
         {/* --------------------------------------------------------------- */}
-        <h2>{editingConversation ? 'Editing Room' : 'Created Room'}</h2>
+        <h2>{editingConversation ? "Editing Room" : "Created Room"}</h2>
         {/* --------------------------------------------------------------- */}
         <form onSubmit={handleSubmitSearch} className={`${style.form}`}>
           <input
-            type='text'
+            type="text"
             autoFocus={true}
             value={state.username}
-            placeholder='insert'
-            onChange={(e) =>
-              setState((prev) => ({ ...prev, username: e.target.value }))
-            }
+            placeholder="insert"
+            onChange={(e) => setState((prev) => ({ ...prev, username: e.target.value }))}
           />
-          <button type='submit' disabled={!state.username}>
-            {loadingSearch ? <SvgLoading size={24} /> : 'Search'}
+          <button type="submit" disabled={!state.username}>
+            {loading ? <SvgLoading size={24} /> : "Search"}
           </button>
         </form>
         {/* --------------------------------------------------------------- */}
-        {dataSearch?.searchUsers && (
-          <UserSearchList
-            users={dataSearch?.searchUsers}
-            setState={setState}
-            state={state}
-          />
+        {data?.searchUsers && (
+          <UserSearchList users={data?.searchUsers} setState={setState} state={state} />
         )}
         {/* --------------------------------------------------------------- */}
         {state.participants.length !== 0 && (
@@ -155,16 +136,12 @@ export const ConversationModal: FC<TypeConversationModal> = ({
               <p>the minimum number of participants is two </p>
             )}
             <button onClick={() => onCreatedRoom(editingConversation)}>
-              {loadingCreateConversation ? (
-                <SvgLoading size={24} />
-              ) : (
-                'Create room'
-              )}
+              {loadingCreateConversation ? <SvgLoading size={24} /> : "Create room"}
             </button>
           </>
         )}
         {/* --------------------------------------------------------------- */}
       </section>
     </div>
-  )
-}
+  );
+};
