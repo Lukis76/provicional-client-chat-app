9d86b44a2c1dda58d2c073e362f30d679c132a89
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-02-26 13:04:44 -0300

    prettier

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,17 +1,17 @@
 // import { Profile } from "@components/perfil";
-import type { FC } from 'react'
-import { LeftBar } from './navbar/leftBar'
-import { useAuthorization } from '@hooks/index'
-import { Navigate, Outlet } from 'react-router-dom'
-import Loading from '@components/utils/loading'
-import css from '@styles/chat/chat.module.css'
+import type { FC } from "react";
+import { LeftBar } from "./navbar/leftBar";
+import { useAuthorization } from "@hooks/index";
+import { Navigate, Outlet } from "react-router-dom";
+import { Loading } from "@components/utils/loading";
+import css from "@styles/chat/chat.module.css";
 
 export const Chat: FC = () => {
-  const { check } = useAuthorization()
+  const { check } = useAuthorization();
   return (
     <>
       {check === false ? (
-        <Navigate to={'/login'} />
+        <Navigate to={"/login"} />
       ) : check === undefined ? (
         <div className={`${css.center_loading}`}>
           <Loading />
@@ -20,9 +20,8 @@ export const Chat: FC = () => {
         <main className={`${css.main}`}>
           <LeftBar />
           <Outlet />
-          
         </main>
       )}
     </>
-  )
-}
+  );
+};
