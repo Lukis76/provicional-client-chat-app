912d3841c8750ac9b77b788b9fa7bbf523096cf0
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-02-26 12:56:59 -0300

    view pass formulario

--- src/assets/svg/view.tsx
+++ src/assets/svg/view.tsx
@@ -1,25 +1,23 @@
-import { SVGProps } from 'react'
+import { SVGProps } from "react";
 
 interface SVG extends SVGProps<SVGSVGElement> {
-  size?: number
+  size?: number;
 }
 
 export const SvgView = (props: SVG) => (
   <svg
-    xmlns='http://www.w3.org/2000/svg'
-    className='icon icon-tabler icon-tabler-eye'
-    width={props.size || '1.5rem'}
-    height={props.size || '1.5rem'}
+    width={props.size || "1.5rem"}
+    height={props.size || "1.5rem"}
     strokeWidth={3}
-    viewBox='0 0 24 24'
-    stroke='currentColor'
-    fill='none'
-    strokeLinecap='round'
-    strokeLinejoin='round'
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
-    <path d='M0 0h24v24H0z' stroke='none' />
-    <path d='M10 12a2 2 0 1 0 4 0 2 2 0 1 0-4 0' />
-    <path d='M22 12c-2.667 4.667-6 7-10 7s-7.333-2.333-10-7c2.667-4.667 6-7 10-7s7.333 2.333 10 7' />
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M10 12a2 2 0 1 0 4 0 2 2 0 1 0-4 0" />
+    <path d="M22 12c-2.667 4.667-6 7-10 7s-7.333-2.333-10-7c2.667-4.667 6-7 10-7s7.333 2.333 10 7" />
   </svg>
-)
+);
