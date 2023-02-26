66e9b767819589c2e199a52b736f31cabd741edd
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-02-26 13:09:27 -0300

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/client'
-import { client } from '@GraphQL/index'
-import { AuthUserProvider } from '@context/index'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@GraphQL/index";
+import { AuthUserProvider } from "@context/index";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 // import es from 'javascript-time-ago/locale/es-AR.json'
 
 // TimeAgo.addDefaultLocale(en)
 TimeAgo.addLocale(en);
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <ApolloProvider client={client}>
     <AuthUserProvider>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
     </AuthUserProvider>
   </ApolloProvider>
-)
+);
