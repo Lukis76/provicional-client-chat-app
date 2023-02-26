a251fa4a1ffb0e12d81f416db27e5f986ce09db7
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-02-26 13:03:11 -0300

    hook control de formularios login register

--- src/hooks/Form.tsx
+++ src/hooks/Form.tsx
@@ -1,95 +1,62 @@
-import { FormEvent, useState, useContext } from 'react'
-import { GraphQLErrors } from '@apollo/client/errors'
-import { authUserContext } from '@context/index'
-import { useNavigate } from 'react-router-dom'
-import { useMutation } from '@apollo/client'
-import { operations } from '@GraphQL/index'
-import useValidator from './useValidator'
-import { toast } from 'react-hot-toast'
-
-type LOGIN = 'LOGIN'
-type REGISTER = 'REGISTER'
-
+import { authUserContext } from "@context/index";
+import { useNavigate } from "react-router-dom";
+import { FormEvent, useContext } from "react";
+import { useValidator } from "./useValidator";
+import { useMutation } from "@apollo/client";
+import { operations } from "@GraphQL/index";
+import { toast } from "react-hot-toast";
+///////////////////////////////////////
+type formType = "LOGIN" | "REGISTER";
+//-----------------------------------
 type initialStateForm = {
-  username?: string | undefined
-  email: string | undefined
-  password: string | undefined
-  confirmPassword?: string | undefined
-}
-
-export const useForm = (
-  typeForm: LOGIN | REGISTER,
-  initialState: initialStateForm
-) => {
-  const navigate = useNavigate()
-  // const [errors, setErrors] = useState<GraphQLErrors>([])
-  const { login } = useContext(authUserContext)
-  //------------------------------------------------------------
-  const { handleChange, errorValues, errors, values } =
-    useValidator(initialState)
-  //------------------------------------------------------------
-
-  //////////////>>>>>> LOGIN <<<<<</////////////////
-  const [loginUser, { loading: LoginLoading }] = useMutation(
-    operations.user.mutation.LOGIN_USER,
-    {
-      async update(_, { data: { loginUser: userData } }) {
-        login(userData)
-      },
-      onError({ graphQLErrors }) {
-        toast.error(
-          'el logeo a fallado porfavor revisa tu conecciona a internet'
-        )
-      },
-      variables: { loginInput: values },
-    }
-  )
-  //////////////>>>>>> REGISTER <<<<<</////////////////
+  username?: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+  confirmPassword?: string | undefined;
+};
+////////////////////////////////////////////////////////////////////////////
+export const useForm = (form: formType, initialState: initialStateForm) => {
+  //------------------------------------------------------------------------
+  const navigate = useNavigate();
+  const { login } = useContext(authUserContext);
+  const { handleChange, errorValues, errors, values } = useValidator(initialState);
+  //===========================>>>>>> LOGIN <<<<<<===============================================//
+  const [loginUser, { loading: LoginLoading }] = useMutation(operations.user.mutation.LOGIN_USER, {
+    update: async (_, { data: { loginUser: userData } }) => {
+      login(userData);
+    },
+    variables: { loginInput: values },
+  });
+  //=================>>>>>> REGISTER <<<<<<======================//
   const [registerUser, { loading: RegisterLoading }] = useMutation(
     operations.user.mutation.REGISTER_USER,
     {
-      onError({ graphQLErrors }) {
-        toast.error(
-          'el registro a fallado porfavor revisa tu conecciona a internet'
-        )
-      },
-      onCompleted() {
-        navigate('/login')
+      onCompleted: () => {
+        navigate("/login");
       },
       variables: { registerInput: values },
     }
-  )
-
-  ///////////////////////////////////////////////////////
-
+  );
+  //---------------------------------------------------------
   const handleSubmit = async (e: FormEvent<HTMLElement>) => {
-    e.preventDefault()
-    if (typeForm === 'LOGIN') {
-      await loginUser()
-      navigate('/chat')
+    e.preventDefault();
+    if (form === "LOGIN") {
+      const res = await loginUser();
+      res.data.loginUser.token !== null
+        ? navigate("/chat")
+        : toast.error("este uruario no existe! \nrevisa tu email y password ");
     }
-    if (typeForm === 'REGISTER') {
-      await registerUser()
+    if (form === "REGISTER") {
+      await registerUser();
     }
-  }
-
-  const select = () => {
-    if (typeForm === 'LOGIN') {
-      return LoginLoading
-    }
-    if (typeForm === 'REGISTER') {
-      return RegisterLoading
-    }
-  }
-
+  };
+  //--------------
   return {
     handleChange,
-    // handleBlur,
     errors,
     errorValues,
     handleSubmit,
     values,
-    // errors: errorValues,
-    loading: select(),
-  }
-}
+    loading: form === "LOGIN" ? LoginLoading : form === "REGISTER" ? RegisterLoading : null,
+  };
+};
