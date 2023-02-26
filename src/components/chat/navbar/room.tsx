641d799419b5ebc601e050c17c3a221810b067bd
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-02-26 14:55:41 -0300

    reutilizaacion de glowMenu

--- src/components/chat/navbar/room.tsx
+++ src/components/chat/navbar/room.tsx
@@ -1,73 +1,73 @@
-import { Dispatch, FC, SetStateAction, useContext, useState } from 'react'
-import { ConversationFE } from '@types'
-import ReactTimeAgo from 'react-time-ago'
-import { useViewConversation } from '@hooks/index'
-import { ContextMenu } from '@components/chat/navbar/modal/contextMenu'
-import { authUserContext } from '@context/index'
-import { useParams } from 'react-router-dom'
-import css from '@styles/chat/navbar/room.module.css'
-import { formatUsersName } from '@components/utils/formatUserName'
+import { Dispatch, FC, SetStateAction, useContext, useState } from "react";
+import { ConversationFE } from "@types";
+import ReactTimeAgo from "react-time-ago";
+import { useOnDeletedConversation, useViewConversation } from "@hooks/index";
+import { authUserContext } from "@context/index";
+import { useParams } from "react-router-dom";
+import css from "@styles/chat/navbar/room.module.css";
+import { formatUsersName } from "@components/utils/formatUserName";
+import { GlowMenu } from "./glowMenu";
 
 type RoomType = {
-  conversation: ConversationFE
-  setEditingConversation: Dispatch<SetStateAction<ConversationFE | null>>
-  setIsOpen: Dispatch<SetStateAction<boolean>>
-}
+  conversation: ConversationFE;
+  setEditingConversation: Dispatch<SetStateAction<ConversationFE | null>>;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
 
 const DEFAUT_IMG =
-  'https://thumbs.dreamstime.com/b/conceptual-hand-writing-showing-breakout-session-business-photo-showcasing-workshop-discussion-presentation-specific-topic-125699196.jpg'
+  "https://thumbs.dreamstime.com/b/conceptual-hand-writing-showing-breakout-session-business-photo-showcasing-workshop-discussion-presentation-specific-topic-125699196.jpg";
 
-export const Room: FC<RoomType> = ({
-  conversation,
-  setEditingConversation,
-  setIsOpen,
-}) => {
+export const Room: FC<RoomType> = ({ conversation, setEditingConversation, setIsOpen }) => {
   //------------------
-  const customImg = ''
-  const { conversationId } = useParams()
-  const { user } = useContext(authUserContext)
-  const { onViewConversation } = useViewConversation()
-  const [contextMenu, setContextMenu] = useState<boolean>(false)
+  const customImg = "";
+  const { conversationId } = useParams();
+  const { user } = useContext(authUserContext);
+  const { onViewConversation } = useViewConversation();
+  const { onConversationDeleted } = useOnDeletedConversation();
+  const [contextMenu, setContextMenu] = useState<boolean>(false);
   //---------------------------------------------------------------
   const getUserParticipant = (conversation: ConversationFE) => {
-    return Boolean(
-      conversation.participants.find((p) => p.user.id === user?.id)
-        ?.hasSeenLatestMsg
-    )
-  }
+    return Boolean(conversation.participants.find((p) => p.user.id === user?.id)?.hasSeenLatestMsg);
+  };
   //---------------------------------------------------------------
   const handleClick = (e: React.MouseEvent) => {
-    if (e.type === 'click') {
-      onViewConversation(
-        conversation.id,
-        getUserParticipant(conversation),
-        user
-      )
-    } else if (e.type === 'contextmenu') {
-      e.preventDefault()
-      setContextMenu(true)
+    if (e.type === "click") {
+      onViewConversation(conversation.id, getUserParticipant(conversation), user);
+    } else if (e.type === "contextmenu") {
+      e.preventDefault();
+      setContextMenu(true);
     }
-  }
+  };
+  //--------------------------------------------------------------
+  const optionsMenu = [
+    {
+      name: "Edit",
+      action: () => {
+        setEditingConversation(conversation);
+        setIsOpen(true);
+        setContextMenu(false);
+      },
+    },
+    {
+      name: "Delete",
+      action: async () => {
+        await onConversationDeleted(conversation.id);
+        setContextMenu(false);
+      },
+    },
+  ];
   //--------------------------------------------------------------
   return (
     <div
-      aria-checked={conversation.id === conversationId ? 'true' : 'false'}
+      aria-checked={conversation.id === conversationId ? "true" : "false"}
       // ${ conversation.id === conversationId && 'bg-zinc-600'}
       className={`${css.room}`}
-      onClick={(e) => {
-        handleClick(e)
-      }}
+      onClick={handleClick}
       onContextMenu={handleClick}
     >
       {/* --------------------------------------------------------------- */}
       {contextMenu && (
-        <ContextMenu
-          close={setContextMenu}
-          conversationId={conversation.id}
-          setEditingConversation={setEditingConversation}
-          conversation={conversation}
-          setIsOpen={setIsOpen}
-        />
+        <GlowMenu style={css.positionMenuUser} options={optionsMenu} view={setContextMenu} />
       )}
       {/* --------------------------------------------------------------- */}
       <div className={`${css.info}`}>
@@ -75,28 +75,25 @@ export const Room: FC<RoomType> = ({
           <img
             aria-checked={getUserParticipant(conversation)}
             src={customImg || DEFAUT_IMG}
-            alt={'img room'}
+            alt={"img room"}
           />
         </div>
         <div className={`${css.expand}`}>
           <div>
-            <p className='text-sm truncate'>
+            <p className="text-sm truncate">
               {formatUsersName(conversation.participants, user?.id as string)}
             </p>
             -
-            <span className='text-xs'>
-              <ReactTimeAgo
-                date={Number(new Date(conversation.updatedAt))}
-                timeStyle={'twitter'}
-              />
+            <span className="text-xs">
+              <ReactTimeAgo date={Number(new Date(conversation.updatedAt))} timeStyle={"twitter"} />
             </span>
           </div>
-          <div className=''>
-            <span className=''>{conversation?.latestMsg?.body}</span>
+          <div className="">
+            <span className="">{conversation?.latestMsg?.body}</span>
           </div>
         </div>
       </div>
       {/* --------------------------------------------------------------- */}
     </div>
-  )
-}
+  );
+};
